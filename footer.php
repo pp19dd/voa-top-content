fdf03e480f9bc5675684f15381c5904517d1e17e
Stephen Mekosh <mekosh@gmail.com>
2016-11-23 23:58:30 -0500

    Added logo to footer and improved styles

--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
 
 <footer>
     <inner>
-        <item>About VOA</item>
-        <item>Contact VOA</item>
-        <item>Media Relations</item>
+        <logo><a href="http://www.voanews.com/"><img src="<?php echo get_template_directory_uri() ?>/img/voa-logo_142x60_2x_f8f8f8.png" height="20" alt="VOA" /></a></logo>
+        <item><a href="#">About VOA</a></item>
+        <item><a href="#">Contact VOA</a></item>
+        <item><a href="#">Media Relations</a></item>
     </inner>
 </footer>
