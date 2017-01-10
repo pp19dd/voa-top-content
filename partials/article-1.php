0c74409ad0e62ad7c8c1856bfb406307ba0bfc0e
Stephen Mekosh <smekosh@DC-IMAC3454-001.local>
2017-01-10 14:40:14 -0500

    Added condition to show video icon or not

--- partials/article-1.php
+++ partials/article-1.php
@@ -6,7 +6,10 @@
             <excerpt><span><span><?php echo $post["excerpt"] ?></span></span></excerpt>
             <continue><span><a href="<?php echo $post["permalink"] ?>">Continue reading</a></span></continue>
         </anchor>
+        
+        <?php if ( get_post_meta( $post['id'], "_voa_show_video_icon", true )) { ?>
         <div class="video-icon"><div class="video-icon-inner"><i class="fa fa-video-camera" aria-hidden="true"></i></div></div>
+        <?php } ?>
         
         <?php if ( $cat = get_the_category( $post['id'] )) { if ( $cat[0]->slug && $cat[0]->slug != 'uncategorized' ) { ?>
         <div class="language-service"><div class="language-service-inner"><?php echo $cat[0]->name; ?></div></div>
