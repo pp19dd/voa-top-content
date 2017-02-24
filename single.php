20760debf6114320d0bdafc05309e7cff3c55863
pp19dd <pp19dd@gmail.com>
2017-02-24 12:18:04 -0500

    Move trim to avoid spaces in redirect

--- single.php
+++ single.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 function voa_redirect() {
 	global $post;
-	
-	$redirect_url = get_post_meta( $post->ID, "_voa_redirect_url", true );
-	
-	if( strlen(trim( $redirect_url )) > 0 ) {
+
+	$redirect_url = trim(get_post_meta( $post->ID, "_voa_redirect_url", true ));
+
+	if( strlen( $redirect_url ) > 0 ) {
 		wp_redirect( $redirect_url.'?src=voa-editor-picks', 302 );
 		exit;
 	}
@@ -28,9 +28,9 @@ $image = voa_top_content_get_image_url($thumbnail_id, "full-width");
 if ( !function_exists( 'voa_the_content') || !is_single() ) {
 	function voa_the_content( $content ) {
 		global $post;
-		
+
 		$dateline = get_post_meta( $post->ID, "_voa_dateline", true );
-		
+
 		if ( $dateline != '' ) {
 			$voa_dateline = '<span class="voa-dateline">'. $dateline .' &mdash; </span>';
 			$content  = $voa_dateline . $content;
@@ -38,7 +38,7 @@ if ( !function_exists( 'voa_the_content') || !is_single() ) {
 
 		return $content;
 	}
-	
+
 	add_filter( 'the_content', 'voa_the_content', 0 );
 }
 
@@ -63,9 +63,9 @@ if ( !function_exists( 'voa_the_content') || !is_single() ) {
 		<article>
 			<content>
 				<section class="content-part article-author">
-					
+
 					<?php $voa_byline = get_post_meta( get_the_ID(), '_voa_byline', true ); ?>
-					
+
 					<?php if ( !$voa_byline ) { ?>
 					<div class="author-avatar"><?php echo get_avatar( get_the_author_meta( 'ID' ), 67 ); ?></div>
 					<?php } ?>
@@ -78,23 +78,23 @@ if ( !function_exists( 'voa_the_content') || !is_single() ) {
 					<div class="author-social"></div>
 
 				</section>
-				
-				
-				<?php get_template_part( "partials/share-buttons" ); ?>
-				
-				
-				<section class="content-part article-body"><?php the_content(); ?></section>
-				
-				
+
+
 				<?php get_template_part( "partials/share-buttons" ); ?>
 
-				
+
+				<section class="content-part article-body"><?php the_content(); ?></section>
+
+
+				<?php get_template_part( "partials/share-buttons" ); ?>
+
+
 				<section class="content-part article-categories">
 					<header><i class="fa fa-tag fa-lg" aria-hidden="true"></i><span class="header-text">Categories</span></header>
 					<?php the_category( ', '); ?>
 				</section>
 
-				
+
 				<section class="content-part article-tags">
 					<header><i class="fa fa-tags fa-lg" aria-hidden="true"></i><span class="header-text">Tags</span></header>
 					<?php the_tags( '', ', ', '' ); ?>
@@ -102,13 +102,13 @@ if ( !function_exists( 'voa_the_content') || !is_single() ) {
 
 
 				<section class="prevnext">
-					
+
 					<section class="content-part adjacent-post previous-post">
 						<div class="adjacent-post-inner">
 						<?php if ( $prev_post = voa_top_content_adjacent_post('previous') ) { ?>
-						
+
 						<div class="adjacent-post-image"><a href="<?php echo $prev_post['permalink']; ?>"><img src="<?php echo $prev_post['image_url']; ?>" /></a></div>
-						
+
 						<div class="adjacent-post-text">
 							<p><a href="<?php echo $prev_post['permalink']; ?>">Previous Post</a></p>
 							<h4><a href="<?php echo $prev_post['permalink']; ?>"><?php echo $prev_post['post_title']; ?></a></h4>
@@ -125,7 +125,7 @@ if ( !function_exists( 'voa_the_content') || !is_single() ) {
 					<section class="content-part adjacent-post next-post">
 						<div class="adjacent-post-inner">
 						<?php if ( $next_post = voa_top_content_adjacent_post('next') ) { ?>
-						
+
 						<div class="adjacent-post-text">
 							<p><a href="<?php echo $next_post['permalink']; ?>">Next Post</a></p>
 							<h4><a href="<?php echo $next_post['permalink']; ?>"><?php echo $next_post['post_title']; ?></a></h4>
@@ -143,7 +143,7 @@ if ( !function_exists( 'voa_the_content') || !is_single() ) {
 
 				</section><!-- end .prevnext -->
 
-				
+
 				<section id="comment-section" class="content-part comments">
 
 					<header>Comments</header>
@@ -153,7 +153,7 @@ if ( !function_exists( 'voa_the_content') || !is_single() ) {
 						<div class="fb-comments" data-href="<?php the_permalink(); ?>" data-width="100%" data-numposts="5"></div>
 
 						<?php /*
-						
+
 						<div id="fb-root"></div>
 						<script>(function(d, s, id) {
 						  var js, fjs = d.getElementsByTagName(s)[0];
@@ -162,7 +162,7 @@ if ( !function_exists( 'voa_the_content') || !is_single() ) {
 						  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=<?php echo FB_APP_ID; ?>";
 						  fjs.parentNode.insertBefore(js, fjs);
 						}(document, 'script', 'facebook-jssdk'));</script>
-						
+
 						*/ ?>
 
 					</div>
@@ -172,9 +172,9 @@ if ( !function_exists( 'voa_the_content') || !is_single() ) {
 			</content>
 			<sidebar>
 				<sidebar-inner>
-					
+
 					<?php /*<div id="comment-shortcut"><a href="#comment-section"><span class="comment-icon"><i class="fa fa-comment fa-2x" aria-hidden="true"></i></span><span class="comment-text"><span class="comment-count"><?php echo $comment_count . ' Comment' . ($comment_count != 1 ? 's' : ''); ?></span> <span class="comment-cta">Join the Discussion</span></span></a></div>*/ ?>
-					
+
 					<?php dynamic_sidebar( 'sidebar_article_right' ); ?>
 
 				</sidebar-inner>
