5662f7984b8930d041928d3e536a09c0fca9e7e9
Stephen Mekosh <smekosh@DC-IMAC3454-001.local>
2017-03-07 16:50:46 -0500

    Added video icon option to small photo and text layout

--- partials/article-2.php
+++ partials/article-2.php
@@ -4,6 +4,10 @@
         	<div class="part-one-img">
         		<?php voa_language_service_tag( $post['id'], true ); ?>
 	        	<a href="<?php echo $post["permalink"] ?>"><img src="<?php echo $image ?>" /></a>
+                
+                <?php if ( get_post_meta( $post['id'], "_voa_show_video_icon", true ) === "yes") { ?>
+                <div class="video-icon"><div class="video-icon-inner"><i class="fa fa-video-camera" aria-hidden="true"></i></div></div>
+                <?php } ?>
 	        </div>
         </part>
         <part class="two">
