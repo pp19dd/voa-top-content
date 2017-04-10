243e1f138e7fcca0e7898e0680c97f0f0e61309f
Stephen Mekosh <smekosh@DC-IMAC3454-001.local>
2017-04-10 16:40:57 -0400

    Added "Subscribe" link to header menu

--- header.php
+++ header.php
@@ -100,8 +100,8 @@ $most_recent = voa_top_content_get_most_recently_published_day();
 			<logo><a href="http://www.voanews.com/<?php echo $voa_metrics_tracking_parameter; ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/voa-logo_142x60_2x_f8f8f8.png" width="71" height="30" border="0" alt="VOA" /></a></logo>
 			<menu>
 				<?php /* <a href="<?php echo home_url( '' ); ?>/about/">About</a> */ ?>
-				<a href="<?php echo home_url( '' ); ?>/archives/">Archives</a>
-				<?php /* <a href="#">Search</a> */ ?>
+				<a class="menu-archives-link" href="<?php echo home_url( '' ); ?>/archives/">Archives</a>
+				<a class="menu-subscribe-link" href="http://www.voanews.com/subscribe.html?src=voa-editor-picks" target="_blank">Subscribe<span class="no-mobile"> to the Newsletter</span></a>
 			</menu>
 		</inner>
 	</header>
