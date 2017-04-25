8d048f5cf5575b0b6c57fc5736da5312dd15ed14
Stephen Mekosh <smekosh@users.noreply.github.com>
2017-04-25 17:21:56 -0400

    Added subhed to basic single posts and more color consistency

--- partials/single-basic-intro.php
+++ partials/single-basic-intro.php
@@ -1,6 +1,7 @@
 
 <header class="single-basic-intro">
 	<h1><?php the_title(); ?></h1>
+	<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mi mi, suscipit et egestas et, condimentum vitae est. Fusce venenatis vestibulum nunc lobortis dignissim. Quisque.</h2>
 	<?php if ( has_post_thumbnail() ) { ?>
 	<div class="featured-img"><?php the_post_thumbnail(); ?></div>
 	<?php } ?> 
