879a3ee22cc8f1c9a3c608c59754fe9dde2b51f2
Stephen Mekosh <smekosh@users.noreply.github.com>
2017-05-08 18:19:28 -0400

    Added WP menus to header/footer and fully styled

--- footer.php
+++ footer.php
@@ -1,21 +1,30 @@
 
-<footer>
-    <inner>
-        <div><a class="voa-logo" href="//www.voanews.com/"><img src="<?php echo get_template_directory_uri() ?>/img/voa-logo_142x60_2x_f8f8f8.png" height="20" alt="VOA" /></a></div>
-        
-        <?php
-        	// wp_nav_menu( array(
-        	// 	'menu' => 'blog_footer_menu'
-        	// ));
-        ?>
-        
-        <item><a href="//www.insidevoa.com/">About VOA</a></item>
-        <item><a href="//www.voanews.com/p/5339.html">Contact VOA</a></item>
-        <item><a href="//www.insidevoa.com/">Media Relations</a></item>
-    </inner>
+<footer class="blog-footer">
+	<div class="blog-footer-inner">
+		<a class="voa-logo" href="//www.voanews.com/"><img src="<?php echo get_template_directory_uri() ?>/img/voa-logo_142x60_2x_f8f8f8.png" height="20" alt="VOA" /></a>
+		
+		<div class="footer-nav-menu"><?php wp_nav_menu( array( 'theme_location' => 'Footer List Menu' ) ); ?></div>
+		
+		<?php /*<div><?php echo bloginfo('description'); ?></div>*/ ?>
+	</div>
 </footer>
 
 <!-- <script async defer src="//assets.pinterest.com/js/pinit.js"></script> -->
+
+<script type="text/javascript">
+	var $j = jQuery.noConflict();
+
+	$j(document).ready(function() {
+		
+		(function() {
+			$j('.blog-nav-trigger').on('click', function() {
+				$j('.blog-nav > .menu').slideToggle( 400 );
+			});
+    	})();
+
+	});
+
+</script>
 
 </body>
 </html>
