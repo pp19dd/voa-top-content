55a75b80cbb4163a3a0569e043f59420a7954100
Stephen Mekosh <smekosh@users.noreply.github.com>
2017-06-30 15:23:35 -0400

    Category page code cleanup

--- category.php
+++ category.php
@@ -31,26 +31,6 @@
 					<?php } ?>
 				</section>
 				
-				<?php /*
-				<section class="archive-teasers content-part">
-
-					<?php while ( have_posts() ) : the_post(); ?>
-						
-						<article class="archive-teaser">
-							<?php 
-							$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'quarter-width-small' );
-							$image = $image[0];
-							?>
-							<div class="teaser-img-container"><a class="teaser-img" href="<?php the_permalink(); ?>" 
-								style="background-image: url(<?php echo $image; ?>);"><?php voa_language_service_tag( $post->ID, true ); ?></a></div>
-							<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-						</article>
-						
-					<?php endwhile; // end of The Loop ?>
-					
-				</section>
-				*/ ?>
-
 			</content>
 			
 			
