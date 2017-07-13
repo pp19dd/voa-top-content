af2248788dac332631b75d82066617f563b51e1c
Stephen Mekosh <smekosh@users.noreply.github.com>
2017-07-13 18:41:45 -0400

    Auto-generate missing images on post excerpts

--- partials/excerpt.php
+++ partials/excerpt.php
@@ -1,7 +1,12 @@
 
 <article class="article-excerpt">
 	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-		<div class="excerpt-img"><?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'quarter-width-short' ); } ?></div>
+		<div class="excerpt-img"><?php 
+			if ( has_post_thumbnail() ) {
+				voa_generate_missing_image_size( get_post_thumbnail_id(), 'quarter-width-short' );
+				the_post_thumbnail( 'quarter-width-short' );
+			} 
+		?></div>
 		<div class="excerpt-text">
 			<h2><?php the_title(); ?></h2>
 			<p class="excerpt-meta">
