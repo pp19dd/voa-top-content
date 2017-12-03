786269967c098322f703b8e3df3c188023424a4c
Stephen Mekosh <smekosh@users.noreply.github.com>
2017-12-02 23:14:13 -0500

    Set Turkish to use 'Open Sans' font family

--- languages/tr.php
+++ languages/tr.php
@@ -1,2 +1,6 @@
 
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&amp;subset=cyrillic,cyrillic-ext,latin-ext" />
+
+<style type="text/css">
+	body, .search-form input { font-family: 'Open Sans', sans-serif; }
+</style>
